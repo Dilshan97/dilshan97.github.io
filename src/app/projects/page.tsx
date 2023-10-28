280c692050692049bb802624e77c82aae0e1fff3
Dilshan Ramesh <dilshanramesh81@gmail.com>
2023-10-28 17:33:55 +0530

    projects contents updated

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -31,56 +31,63 @@ const Page: FC = () => {
           <div className="grid gap-4">
             <ProjectCard
               title={'Dominos Pizza Clone App'}
-              intro={'Jobriver is a Polish employment platform. It often helps developers to find a job that suits them easily by filtering!'}
+              intro={"The Flutter-powered Domino's Pizza Clone App is a sophisticated mobile application that mirrors the renowned Domino's Pizza ordering experience. Leveraging the versatility of Flutter, it ensures a smooth and consistent user experience across both Android and iOS platforms."}
               image={dominos}
             />
+
+            <ProjectCard
+              title={'Coffee Shop App'}
+              intro={'A concept mobile app for a coffee shop and built with Flutter, brings the charm of a local cafe to your fingertips. This mobile application, compatible with both Android and iOS, offers a seamless experience for coffee enthusiasts.'}
+              image={coffee}
+            />
+
+            <ProjectCard
+              title={'Weather App'}
+              intro={"The Weather App crafted with Flutter is a sleek and efficient mobile application designed to provide up-to-date weather information at your fingertips. Built using Flutter's versatile framework, it ensures a seamless experience on both Android and iOS platforms."}
+              image={weather}
+            />
+
+            <ProjectCard
+              title={'Grocery Shopping App'}
+              intro={'A concept for user onboarding for Grocery Shopping App which is developed using flutter. The onboarding flow including authenication and welcome screens for seemless user experince.'}
+              image={eCommerce}
+            />
+          </div>
+          <div className="grid gap-4 sm:pt-40">
+            <ProjectCard
+              title={'Facebook Clone App'}
+              intro={"The Facebook Clone App developed with Flutter is a dynamic mobile application that replicates the renowned Facebook's key functionalities. Leveraging the power of Flutter, it ensures a seamless experience on both Android and iOS devices. "}
+              image={facebook}
+            />
+
+            <ProjectCard
+              title={'Tour Planner App'}
+              intro={'The Tour Planner App, concept mobile application which is powered by Flutter. This mobile application simplifies trip planning & all in one activities.'}
+              image={tourPlanner}
+            />
+
+            <ProjectCard
+              title={'Shopping Cart App'}
+              intro={'This is part of a E-commerce app which contain the all inclusive Shopping Cart build with Flutter and dart. Streamline your shopping experience with this efficient and modern Flutter-based app.'}
+              image={shoppingCart}
+            />
+
             {/* <ProjectCard
               title={'Keells Super Clone App'}
               intro={'Jobriver is a Polish employment platform. It often helps developers to find a job that suits them easily by filtering!'}
               image={image}
             /> */}
-            <ProjectCard
-              title={'Shopping Cart App'}
-              intro={'Jobriver is a Polish employment platform. It often helps developers to find a job that suits them easily by filtering!'}
-              image={shoppingCart}
-            />
-            <ProjectCard
-              title={'Weather App'}
-              intro={'Jobriver is a Polish employment platform. It often helps developers to find a job that suits them easily by filtering!'}
-              image={weather}
-            />
-            <ProjectCard
-              title={'Facebook Clone App'}
-              intro={'Jobriver is a Polish employment platform. It often helps developers to find a job that suits them easily by filtering!'}
-              image={facebook}
-            />
-          </div>
-          <div className="grid gap-4 sm:pt-40">
-            <ProjectCard
-              title={'Grocery Shopping App'}
-              intro={'Jobriver is a Polish employment platform. It often helps developers to find a job that suits them easily by filtering!'}
-              image={eCommerce}
-            />
-            <ProjectCard
-              title={'Tour Planner App'}
-              intro={'Jobriver is a Polish employment platform. It often helps developers to find a job that suits them easily by filtering!'}
-              image={tourPlanner}
-            />
-            <ProjectCard
-              title={'Coffee Shop App'}
-              intro={'Jobriver is a Polish employment platform. It often helps developers to find a job that suits them easily by filtering!'}
-              image={coffee}
-            />
+
             {/* <ProjectCard
               title={'Tesla Car App'}
               intro={'Jobriver is a Polish employment platform. It often helps developers to find a job that suits them easily by filtering!'}
               image={image}
             /> */}
-            <ProjectCard
+            {/* <ProjectCard
               title={'Wasthi Production App'}
               intro={'Jobriver is a Polish employment platform. It often helps developers to find a job that suits them easily by filtering!'}
               image={wasthiProduction}
-            />
+            /> */}
           </div>
         </div>
       </div>
