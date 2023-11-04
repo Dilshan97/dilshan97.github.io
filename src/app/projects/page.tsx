07808db60a5c86b2796eef50d504744dcd2f8664
Dilshan Ramesh <dilshanramesh81@gmail.com>
2023-11-04 23:35:46 +0530

    project ui updated

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -28,43 +28,50 @@ const Page: FC = () => {
     {
       "title": "Shopping Cart App",
       "intro": "This is part of a E-commerce app which contain the all inclusive Shopping Cart build with Flutter and dart. Streamline your shopping experience with this efficient and modern Flutter-based app.",
-      "image": shoppingCart
+      "image": shoppingCart,
+      "url": "https://github.com/Dilshan97/Flutter-Provider-Shopping-Cart"
     },
     {
       "title": "Weather App",
       "intro": "The Weather App crafted with Flutter is a sleek and efficient mobile application designed to provide up-to-date weather information at your fingertips. Built using Flutter's versatile framework, it ensures a seamless experience on both Android and iOS platforms.",
-      "image": weather
+      "image": weather,
+      "url": "https://github.com/Dilshan97/Flutter-Weather-App"
     },
     {
       "title": "Dominos Pizza Clone App",
       "intro": "The Flutter-powered Domino's Pizza Clone App is a sophisticated mobile application that mirrors the renowned Domino's Pizza ordering experience. Leveraging the versatility of Flutter, it ensures a smooth and consistent user experience across both Android and iOS platforms.",
-      "image": dominos
+      "image": dominos,
+      "url": "https://github.com/Dilshan97/Flutter-Dominos-Pizza-Clone-App"
     },
     {
       "title": "Coffee Shop App",
       "intro": "A concept mobile app for a coffee shop and built with Flutter, brings the charm of a local cafe to your fingertips. This mobile application, compatible with both Android and iOS, offers a seamless experience for coffee enthusiasts.",
-      "image": coffee
+      "image": coffee,
+      "url": "https://github.com/Dilshan97/Flutter-Coffee-Shop-App-UI"
     },
 
     {
       "title": "Grocery Shopping App",
       "intro": "A concept for user onboarding for Grocery Shopping App which is developed using flutter. The onboarding flow including authenication and welcome screens for seemless user experince.",
-      "image": eCommerce
+      "image": eCommerce,
+      "url": "https://github.com/Dilshan97/Flutter-Grocery-Shopping-App"
     },
     {
       "title": "Facebook Clone App",
       "intro": "The Facebook Clone App developed with Flutter is a dynamic mobile application that replicates the renowned Facebook's key functionalities. Leveraging the power of Flutter, it ensures a seamless experience on both Android and iOS devices.",
-      "image": facebook
+      "image": facebook,
+      "url": "https://github.com/Dilshan97/Flutter-Facebook-UI"
     },
     {
       "title": "Tour Planner App",
       "intro": "The Tour Planner App, concept mobile application which is powered by Flutter. This mobile application simplifies trip planning & all in one activities.",
-      "image": tourPlanner
+      "image": tourPlanner,
+      "url": "https://github.com/Dilshan97/Island-tour-planner"
     }
   ];
 
   return (
-    <div className="projects bg-white-50">
+    <div className="projects bg-slate-100	">
       <div className="container mx-auto max-w-5xl">
         <div className="mx-2 max-sm:mx-5">
           <p className="text-4xl py-4 text-gray-600 max-sm:text-xl">Projects</p>
@@ -85,7 +92,7 @@ const Page: FC = () => {
           }}
           initial="hidden"
           animate="show"
-          className="grid grid-cols-2 md:grid-cols-2 max-sm:grid-cols-1 gap-6 py-10"
+          className="lg:pt-[60px] md:pt-[40px] pt-[24px] flex flex-col gap-[24px] md:gap-[40px]  aos-init aos-animate"
         >
           <Suspense fallback={<ProjectsListLoading />}>
             {projects.map((project: ProjectModel) => <ProjectCard project={project} />)}
