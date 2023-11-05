7504a15b5052614a08dbdbb063907b727b82c361
Dilshan Ramesh <dilshanramesh81@gmail.com>
2023-11-05 12:13:47 +0530

    projects ui updated

--- src/app/projects/components/projectCard.tsx
+++ src/app/projects/components/projectCard.tsx
@@ -10,8 +10,9 @@ import { ProjectImageLoading } from '@/app/projects/components/ProjectsListLoadi
 import ProjectModel from '@/utils/models/project.model';
 interface ProjectCardProps {
   project: ProjectModel;
+  direction: string;
 }
-const ProjectCard: FC<ProjectCardProps> = ({ project }) => {
+const ProjectCard: FC<ProjectCardProps> = ({ project, direction }) => {
 
   const [isLoaded, setLoaded] = useState<boolean>(false);
 
@@ -33,32 +34,44 @@ const ProjectCard: FC<ProjectCardProps> = ({ project }) => {
         },
       }}
       className="bg-white flex flex-col md:flex-row md:gap-[32px] lg:gap-0 lg:items-center lg:justify-between shadow-card-smooth py-[40px] px-[40px] rounded-[4px]">
-      
+
+      {direction == "left" && (
+        <div className="flex pt-[24px] md:pt-0 md:pr-5">
+          <Image
+            src={project.image}
+            alt={project.title}
+            width={500}
+            height={500}
+          />
+        </div>
+      )}
+
       <div className="flex flex-col gap-[16px] md:w-[400px] lg:w-[unset] md:gap-[32px]">
         <div className="flex flex-col gap-[8px] md:gap-[16px]">
           <h5 className="md:text-2xl text-3xl font-bold">{project.title}</h5>
           <p className="text-base text-N-600 lg:w-[500px] leading-normal">{project.intro}</p>
         </div>
         <div className="flex gap-[16px] flex-col md:flex-row">
-          <a 
-          href={project.url ?? '#'} 
-          rel="noreferrer"
-          target="_blank"
-          className="button items-center flex justify-center gap-[8px] cursor-pointer bg-B-50 rounded-[8px] hover:bg-B-200 border border-B-600 px-[20px] py-[8px] text-base font-500 w-full md:w-[160px]">
-           <i className="ri-github-fill"></i> Live Demo
+          <a
+            href={project.url ?? '#'}
+            rel="noreferrer"
+            target="_blank"
+            className="button items-center flex justify-center gap-[8px] cursor-pointer bg-B-50 rounded-[8px] hover:bg-B-200 border border-B-600 px-[20px] py-[8px] text-base font-500 w-full md:w-[160px]">
+            <i className="ri-github-fill"></i> Live Demo
           </a>
         </div>
       </div>
 
-      <div className="flex pt-[24px] md:pt-0">
-        <Image
-          src={project.image}
-          alt={project.title}
-          width={500}
-          height={500}
-          onLoad={onLoad}
-        />
-      </div>
+      {direction == "right" && (
+        <div className="flex pt-[24px] md:pt-0">
+          <Image
+            src={project.image}
+            alt={project.title}
+            width={500}
+            height={500}
+          />
+        </div>
+      )}
     </motion.div>
   )
 }
